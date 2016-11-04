08e9556d023146840e41fc900fde9413ca9bfa40
Piotr Wyrobek <piotr.wyrobek@williamhill.com>
2016-11-04 10:43:48 +0100

    Fix

--- flow-typed/npm/restify.js
+++ flow-typed/npm/restify.js
@@ -4,7 +4,7 @@ import http from 'http';
 
 declare module restify {
     declare class MyClass extends http.Server {
-        this_exists_so_its_file(): *;
+        this_exists_so_its_fine(): *;
     }
 
     declare function createServer(): MyClass;
