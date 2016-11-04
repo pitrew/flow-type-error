755e6758d694cff32b8b8718b025adee31e35307
Piotr Wyrobek <piotr.wyrobek@williamhill.com>
2016-11-04 10:16:42 +0100

    Annotated server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+// @flow
+
 import {
     createServer,
     MyClass,
 } from 'restify';
 
 export default function start(): void {
-    const s: Server = createServer(options);
+    const s: MyClass = createServer();
 
-    s.this_exists_so_its_file();
+    s.this_exists_so_its_fine();
     s.this_does_not_exist(); //This line is passing too!!
 }
